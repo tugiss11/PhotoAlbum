aca0c93da140a21b1ecbd4ca1b260ac4ef0e38f5
chloetina <guoliang1222@gmail.com>
2014-01-16 19:58:36 +0200

    ok

--- gallery/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from models import *
-
-class ImageURLForm(ModelForm):
-    class Meta:
-        model = AlbumImage
-        fields = ["url"]
--- gallery/layouts/layout_info.py
+++
@@ -1,5 +0,0 @@
-#Generated file, DON'T EDIT! Run generate_layout_list.py instead!
-NAME_DB_MAX_LENGTH = 20
-PAGE_LAYOUTS = (
-   ('default', 'default.css'),
-)
