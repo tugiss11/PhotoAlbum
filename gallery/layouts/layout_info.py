28fbf758f51d14b300f2180944a68850b562a40d
chloetina <guoliang1222@gmail.com>
2014-02-03 14:22:58 +0200

    without the register layout

--- gallery/layouts/layout_info.py
+++ gallery/layouts/layout_info.py
@@ -2,5 +2,11 @@
 NAME_DB_MAX_LENGTH = 20
 PAGE_LAYOUTS = (
    ('default', 'default.css'),
+   ('circle', 'circle.css'),
    ('3pics', '3pics.css'),
+   ('3pics_circle', '3pics_circle.css'),
+   ('5pics', '5pics.css'),
+   ('5pics_circle', '5pics_circle.css'),
+   ('7pics', '7pics.css'),
+   ('7pics_circle', '7pics_circle.css'),
 )
