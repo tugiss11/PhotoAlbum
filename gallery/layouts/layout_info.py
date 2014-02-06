16a5f40edc9dc31ccdb4e701317d27ee982e24b6
Joona Heinikoski <joona@Peruna.local>
2014-02-06 17:18:40 +0200

    Modified readme for final submission. Added edit mode toggle. Added one new layout.

--- gallery/layouts/layout_info.py
+++ gallery/layouts/layout_info.py
@@ -1,12 +1,13 @@
 #Generated file, DON'T EDIT! Run generate_layout_list.py instead!
 NAME_DB_MAX_LENGTH = 20
 PAGE_LAYOUTS = (
-   ('default', 'default.css'),
-   ('circle', 'circle.css'),
    ('3pics', '3pics.css'),
    ('3pics_circle', '3pics_circle.css'),
    ('5pics', '5pics.css'),
    ('5pics_circle', '5pics_circle.css'),
    ('7pics', '7pics.css'),
    ('7pics_circle', '7pics_circle.css'),
+   ('big_and_small', 'big_and_small.css'),
+   ('circle', 'circle.css'),
+   ('default', 'default.css'),
 )
